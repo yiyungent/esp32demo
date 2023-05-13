9888f57845e181330dcc633515f87d6453a3b53a
yiyun <yiyungent@gmail.com>
2023-05-13 13:13:23 +0800

    feat(readme.md,platformio.ini,src/main.cpp): update

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,57 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include "Arduino.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+const char *id = "ffjfifjfjfjfjfjf"; // 定义两个字符串指针常量
+const char *psw = "1478963ZXCV";
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+#define LED_BUILTIN 4
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  // pinMode(LED_BUILTIN, OUTPUT);
-  // pinMode(2,OUTPUT);//设置GPIO 2的模式为：输出模式
-  Serial.begin(4800);//设置串口输出波特率
+  Serial.begin(115200);
+
+  //  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // 设置GPIO 2的模式为：输出模式
+
+  WiFi.begin(id, psw);
+  while (WiFi.status() != WL_CONNECTED)
+  { // 未连接上
+    delay(500);
+    Serial.println("正在连接...");
+  }
+  Serial.println("连接成功！");
+
+  // put your setup code here, to run once:
   Serial.println("setup");
 }
 
 void loop()
 {
-  // digitalWrite(2,HIGH);//把GPIO 2设置为高电平，让灯熄灭。
-  // while (true)
-  // {
-  //   Serial.println("Hello ");
-  // }
-  
-  Serial.println("LED mie");//串口输出内容
-  // delay(200);//阻塞延时200MS
-  // digitalWrite(2,LOW);//把GPIO 2设置为高电平，让灯亮起。
-  // Serial.println("LED灯亮。");//串口输出内容
-  // delay(200);//阻塞延时200MS
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello ");
 
-  
+  //  digitalWrite(LED_BUILTIN, HIGH); //把GPIO 2设置为高电平，让灯熄灭。
+  //  delay(200);//阻塞延时200MS
+  //  digitalWrite(LED_BUILTIN, LOW); //把GPIO 2设置为高电平，让灯亮起。
+  //  Serial.println("LED灯亮。");//串口输出内容
+  //  delay(200);//阻塞延时200MS
+
+  HTTPClient http;
+  http.begin("https://visitor_badge.deta.dev/?pageID=github.Lete114"); // Specify destination for HTTP request
+  // start get
+  int http_code = http.GET();
+  Serial.println(http_code);
+  // handle http code
+  if (http_code != HTTP_CODE_OK)
+  {
+    // get fail.
+    Serial.printf("GET fail, http code is %s\n", http.errorToString(http_code).c_str());
+    return;
+  }
+
+  // http response
+  String response = http.getString();
+  Serial.printf("response:[%s]\n", response.c_str());
+  // close http
+  http.end();
 }
