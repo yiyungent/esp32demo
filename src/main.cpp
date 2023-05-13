9be47ac8c3c7291315b8e810e3f050f8392fa68e
yiyun <yiyungent@gmail.com>
2023-05-13 15:54:20 +0800

    feat(platformio.ini,src/main.cpp): 初步

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,87 @@
 #include "Arduino.h"
 #include <WiFi.h>
 #include <HTTPClient.h>
-const char *id = "ffjfifjfjfjfjfjf"; // 定义两个字符串指针常量
-const char *psw = "1478963ZXCV";
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #define LED_BUILTIN 4
+
+const char *wifiSsid = "ffjfifjfjfjfjfjf";
+const char *wifiPassword = "1478963ZXCV";
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
+const char *mqttServer = "your_MQTT_server";
+const int mqttPort = 1883;
+const char *mqttUser = "your_MQTT_user";
+const char *mqttPassword = "your_MQTT_password";
+
+WiFiClient wifiClient;
+PubSubClient pubSubClient(wifiClient);
+
+void LEDUp()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void LEDDown()
+{
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+void initWiFi()
+{
+  LEDUp();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifiSsid, wifiPassword);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  LEDDown();
+  Serial.println("Connected to WiFi");
+  Serial.print("WiFi.localIP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("WiFi.RRSI: ");
+  Serial.println(WiFi.RSSI());
+}
 
 void setup()
 {
   Serial.begin(115200);
-
-  //  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT); // 设置GPIO 2的模式为：输出模式
-
-  WiFi.begin(id, psw);
-  while (WiFi.status() != WL_CONNECTED)
-  { // 未连接上
-    delay(500);
-    Serial.println("正在连接...");
-  }
-  Serial.println("连接成功！");
-
-  // put your setup code here, to run once:
   Serial.println("setup");
+  
+  // 设置 GPIO 4 的模式为: 输出模式
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  initWiFi();
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello ");
+  Serial.println("loop");
 
-  //  digitalWrite(LED_BUILTIN, HIGH); //把GPIO 2设置为高电平，让灯熄灭。
-  //  delay(200);//阻塞延时200MS
-  //  digitalWrite(LED_BUILTIN, LOW); //把GPIO 2设置为高电平，让灯亮起。
-  //  Serial.println("LED灯亮。");//串口输出内容
-  //  delay(200);//阻塞延时200MS
-
-  HTTPClient http;
-  http.begin("https://visitor_badge.deta.dev/?pageID=github.Lete114"); // Specify destination for HTTP request
-  // start get
-  int http_code = http.GET();
-  Serial.println(http_code);
-  // handle http code
-  if (http_code != HTTP_CODE_OK)
+#pragma region 定时重连 WiFi
+  unsigned long currentMillis = millis();
+  // 定时重连
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval))
   {
-    // get fail.
-    Serial.printf("GET fail, http code is %s\n", http.errorToString(http_code).c_str());
-    return;
+    LEDUp();
+    Serial.print(millis());
+    Serial.println("Reconnecting to WiFi...");
+    WiFi.disconnect();
+    WiFi.reconnect();
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      LEDDown();
+      Serial.print("WiFi.localIP: ");
+      Serial.println(WiFi.localIP());
+    }
+    previousMillis = currentMillis;
   }
+#pragma endregion
 
-  // http response
-  String response = http.getString();
-  Serial.printf("response:[%s]\n", response.c_str());
-  // close http
-  http.end();
+
 }
